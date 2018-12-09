631d07e80a2fd50a1006cd4c7fac9c96126eb2f1
Chandira Withanage <withana587@gmail.com>
2018-12-09 11:36:55 -0600

    added new code

--- src/com/chandira/demo/hibernate/DeleteCourseReviewsDemo.java
+++ src/com/chandira/demo/hibernate/DeleteCourseReviewsDemo.java
@@ -10,7 +10,7 @@ import com.chandira.demo.hibernate.entity.InstructorDetail;
 import com.chandira.demo.hibernate.entity.Review;
 
 
-public class CreateReviewsDemo {
+public class DeleteCourseReviewsDemo {
 
 	public static void main(String[] args) {
 		// create session factory
@@ -27,19 +27,20 @@ public class CreateReviewsDemo {
 		
 		try {
 			
-			// create the course
-			Course course = new Course("Learning trojan");
-			
-			// add reviews
-			course.addReview(new Review("This is going to be a awesome class"));
-			course.addReview(new Review("Wow! I love it"));
-			course.addReview(new Review("I would take this class again"));
 		
 			// begin the transaction
 			session.beginTransaction();
+
+			// get the course
+			Course course = session.get(Course.class, 10);
 			
+			// get the reviews
+			System.out.println(course);
+			
+			System.out.println(course.getReviews());
 			// save the instructor
-			session.save(course);
+			// session.save();
+			session.delete(course);
 			
 			// commit the transaction
 			session.getTransaction().commit();
