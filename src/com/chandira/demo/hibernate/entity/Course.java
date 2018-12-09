3292277910694a0f2968bd02dd34d2390b725e57
Chandira Withanage <withana587@gmail.com>
2018-12-09 11:55:11 -0600

    added many to many features to the entity classes

--- src/com/chandira/demo/hibernate/entity/Course.java
+++ src/com/chandira/demo/hibernate/entity/Course.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -34,6 +36,11 @@ public class Course {
 	@OneToMany(fetch=FetchType.LAZY, cascade = { CascadeType.ALL })
 	@JoinColumn(name="course_id")
 	private List<Review> reviews;
+	
+	@ManyToMany(fetch=FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinTable(name = "course_student",
+	joinColumns=@JoinColumn(name="course_id"), inverseJoinColumns=@JoinColumn(name="student_id"))
+	private List<Student> students;
 
 
 	public Course() {
@@ -86,6 +93,26 @@ public class Course {
 		}
 		
 		reviews.add(theReview);
+	}
+	
+	// add student
+	
+	public void addStudent(Student student)
+	{
+		if(students == null)
+		{
+			students = new ArrayList<>();
+		}
+		
+		students.add(student);
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
 	}
 
 	@Override
--- src/com/chandira/demo/hibernate/entity/Student.java
+++ src/com/chandira/demo/hibernate/entity/Student.java
@@ -1,10 +1,17 @@
 package com.chandira.demo.hibernate.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -25,6 +32,9 @@ public class Student {
 	@Column(name="email")
 	private String email;
 	
+	@ManyToMany(fetch=FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinTable(name="course_student", joinColumns=@JoinColumn(name="student_id"), inverseJoinColumns=@JoinColumn(name="course_id"))
+	private List<Course> course;
 	
 	public Student() {
 	
@@ -67,6 +77,14 @@ public class Student {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Course> getCourse() {
+		return course;
+	}
+
+	public void setCourse(List<Course> course) {
+		this.course = course;
 	}
 
 	@Override
